8bd22ed8d7f6f9b9b4830ce647db6d7e4cbe730a
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-07-08 17:55:31 -0700

    add /hello post endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,19 @@ import express from 'express'
 
 const app = express(); 
 
+// GET -
+// PUT -
+// POST 
+// DELETE
+
+
+app.post("/hello", (req, res, next) => {
+  return res.send("Hello");
+});
 
 app.listen(5050, () => console.log("Server Open"));
+
+
 
 
 
