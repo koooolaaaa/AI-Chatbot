f52b0133723e34b8b66edc0401a216f0ac2593d3
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-07-15 14:00:09 -0700

    add duplicate email check in signup and stub userLogin controller

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -17,10 +17,23 @@ export const userSignup = async (req: Request, res: Response, next: NextFunction
     try {
 
         const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(401).send("User already registered");
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
         return res.status(201).json({ message: "OK", id: user._id.toString() });
+    } catch (error) {
+        console.log(error); 
+        return res.status(200).json({message: "ERROR", cause: error.message});
+    }
+};
+
+export const userLogin = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        // return res.status(201).json({ message: "OK", id: user._id.toString() });
     } catch (error) {
         console.log(error); 
         return res.status(200).json({message: "ERROR", cause: error.message});
