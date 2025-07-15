624c2233d227ce773f14f35584dd13b73dace3e9
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-07-15 13:52:35 -0700

    wire loginValidator to /login route with placeholder controller

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
-import { signupValidator, validate } from "../utils/validators.js"; 
+import { loginValidator, signupValidator, validate } from "../utils/validators.js"; 
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
 userRoutes.post("/signup", validate(signupValidator), userSignup);
-userRoutes.post("/login", validate(signupValidator), userSignup);
+userRoutes.post("/login", validate(loginValidator), userSignup);
 
 export default userRoutes;
