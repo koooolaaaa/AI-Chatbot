14923384bcca3cd4eaadfaca4f43fa402f4eb4e0
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-07-15 14:04:00 -0700

    complete userLogin controller with password verification and proper responses

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
-import { hash } from "bcrypt"
+import { hash, compare } from "bcrypt"
 
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -33,7 +33,14 @@ export const userLogin = async (req: Request, res: Response, next: NextFunction)
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
-        // return res.status(201).json({ message: "OK", id: user._id.toString() });
+        if (!user) {
+            return res.status(401).send("User not registered");
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect Password");
+        }       
+        return res.status(201).json({ message: "OK", id: user._id.toString() });
     } catch (error) {
         console.log(error); 
         return res.status(200).json({message: "ERROR", cause: error.message});
